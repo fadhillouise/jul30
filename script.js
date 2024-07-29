914101f25caec629267391518fb8060a02f6f734
fadhillouise <fadhillouise@gmail.com>
2024-07-29 14:18:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.nav').classList.toggle('active');
+});
